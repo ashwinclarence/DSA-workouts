95125e5713bb8d13ff96e9487e13d517a3b859ea
ASHWIN <meashwinclarence@gmail.com>
2024-07-04 11:25:00 +0530

    question 1

---
+++ fibonacci.js
@@ -0,0 +1,17 @@
+
+
+
+// create a function for getting the fibonacci series
+function fibonacci(n) {
+
+    // add the first two numbers
+    const fib = [0, 1]
+
+    // find the rest of the numbers in a loop
+    for (let i = 2; i < n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2]
+    }
+    return fib
+}
+
+console.log(fibonacci(10));
